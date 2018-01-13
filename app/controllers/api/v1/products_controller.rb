51d39450f7fb32dd684462b593b9c15f1dc5ea97
RaviMariya-MTAJ <ravi.mariya@mtajsolutions.com>
2018-01-13 15:58:24 +0530

    created api of products crud

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,34 @@
 class Api::V1::ProductsController < ApplicationController
+  before_action :set_product, only: [:update, :destroy]
+
   def index
     @products = Product.all
-    render json: @products
+    render json: @products, status: :ok
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    render json: @product, status: :created
+  end
+
+  def update
+    @product.update(product_params)
+    render json: @product, status: :updated
+  end
+
+  def destroy
+    @product.destroy
+    render json: {status: :deleted}
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price)
   end
 end
